15d5a06c107674389d09ef33c753a9f617fd862a
Yoonju Ahn <ju8559@naver.com>
2023-12-08 16:41:42 +0900

    #1 - 프로젝트 초기세팅
    *개발 환경
    IDE: InteliJ IDEA 2023.2.3 (Ultimate Edition)
    Java: 17
    Spring Boot: 3.2.0
    Dependencies: Spring Web, Spring Data JPA, Lombok, MySQL Driver
    
    python으로 가공한 데이터 추가 완료

---
+++ src/test/java/com/teamB/sulijoa_be/SulIjoaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamB.sulijoa_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SulIjoaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
