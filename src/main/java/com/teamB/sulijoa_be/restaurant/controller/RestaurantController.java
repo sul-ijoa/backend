ea37fde539e9d7981d682c0c0ddb5c3658bb1caf
Yoonju Ahn <ju8559@naver.com>
2023-12-20 11:02:01 +0900

    #5 - /all, /info 앤드포인트만 사용
    프론트에서 /all, /info에서 response를 모든 컬럼을 받아서 /detail-info에서 사용하기로함

--- src/main/java/com/teamB/sulijoa_be/restaurant/controller/RestaurantController.java
+++ src/main/java/com/teamB/sulijoa_be/restaurant/controller/RestaurantController.java
@@ -3,10 +3,7 @@ package com.teamB.sulijoa_be.restaurant.controller;
 import com.teamB.sulijoa_be.restaurant.repository.dto.RestaurantDto;
 import com.teamB.sulijoa_be.restaurant.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,17 +19,17 @@ public class RestaurantController {
 	}
 
 	/**
-	 * 모든 레스토랑 정보를 가져오는 메서드
+	 * 모든 레스토랑을 가져오는 메서드
 	 *
 	 * @return 레스토랑 목록에 대한 응답 DTO
 	 */
 	@GetMapping("/all")
-	public List<RestaurantDto.AllInfoResponse> getAllRestaurants() {
+	public List<RestaurantDto> getAllRestaurants() {
 		return restaurantService.getAllRestaurants();
 	}
 
 	/**
-	 * 조건에 따라 레스토랑 정보를 가져오는 메서드
+	 * 조건에 따라 레스토랑을 가져오는 메서드
 	 *
 	 * @param category        카테고리
 	 * @param underSojuPrice  소주 가격 기준 (미만)
@@ -42,23 +39,12 @@ public class RestaurantController {
 	 * @return 조건에 맞는 레스토랑 목록에 대한 응답 DTO
 	 */
 	@GetMapping("/info")
-	public List<RestaurantDto.AllInfoResponse> getRestaurantsByCondition(
+	public List<RestaurantDto> getRestaurantsByCondition(
 					@RequestParam(name = "category", required = false, defaultValue = "") String category,
 					@RequestParam(name = "underSojuPrice", required = false) Integer underSojuPrice,
 					@RequestParam(name = "moreSojuPrice", required = false) Integer moreSojuPrice,
 					@RequestParam(name = "underBeerPrice", required = false) Integer underBeerPrice,
 					@RequestParam(name = "moreBeerPrice", required = false) Integer moreBeerPrice) {
 		return restaurantService.getRestaurantsByCondition(category, underSojuPrice, moreSojuPrice, underBeerPrice, moreBeerPrice);
-	}
-
-	/**
-	 * 특정 레스토랑에 대한 상세 정보를 가져오는 메서드
-	 *
-	 * @param restaurant_seq 레스토랑 일련번호
-	 * @return 레스토랑 상세 정보에 대한 응답 DTO
-	 */
-	@GetMapping("/detail-info")
-	public RestaurantDto.DetailResponse getRestaurantDetail(@RequestParam(name = "restaurant_seq") Long restaurant_seq) {
-		return restaurantService.getRestaurantDetail(restaurant_seq);
 	}
 }
